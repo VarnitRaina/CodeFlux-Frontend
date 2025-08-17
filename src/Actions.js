265b8f64567dca585b91c2e7664eeed237733faf
varnit raina <varnitraina@gmail.com>
2025-08-18 02:08:04 +0530

    Add Actions.js back to frontend

---
+++ src/Actions.js
@@ -0,0 +1,10 @@
+const ACTIONS={
+    JOIN:"join",
+    JOINED:"joined",
+    DISCONNECTED:"disconnected",
+    LEAVE:"leave",
+    SYNC_CODE:"sync_code",
+    CODE_CHANGE:"code_change",
+}
+
+module.exports=ACTIONS;
