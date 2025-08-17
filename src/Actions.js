3971b36b26a98508808c1fae2fc0afe1cf813ffd
varnit raina <varnitraina@gmail.com>
2025-08-18 03:16:55 +0530

    actions

--- src/Actions.js
+++ src/Actions.js
@@ -1,10 +1,10 @@
-const ACTIONS={
-    JOIN:"join",
-    JOINED:"joined",
-    DISCONNECTED:"disconnected",
-    LEAVE:"leave",
-    SYNC_CODE:"sync_code",
-    CODE_CHANGE:"code_change",
-}
-
-module.exports=ACTIONS;
+const ACTIONS = {
+  JOIN: 'join',
+  JOINED: 'joined',
+  DISCONNECTED: 'disconnected',
+  CODE_CHANGE: 'code-change',
+  SYNC_CODE: 'sync-code',
+  LEAVE: 'leave',
+  SYNC_CLIENTS: 'sync-clients'  // Add this line
+};
+module.exports = ACTIONS;
